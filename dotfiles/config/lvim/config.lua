f903633e31f8fdd89d2d6c33d416528921f071b0
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2023-11-11 18:23:28 +0100

    autoselect python environment

--- dotfiles/config/lvim/config.lua
+++ dotfiles/config/lvim/config.lua
@@ -1,6 +1,5 @@
 lvim.leader = ","
 lvim.colorscheme = "solarized-flat"
-lvim.format_on_save.enabled = true
 
 local opts = { silent = true }
 vim.keymap.set("n", "<Up>", ":bnext<CR>", opts)
@@ -8,6 +7,37 @@ vim.keymap.set("n", "<Down>", ":bprevious<CR>", opts)
 
 vim.opt.guifont = "Inconsolata Nerd Font Mono:h18"
 vim.opt.background = "light"
+
+-- improve python highlighting
+lvim.builtin.treesitter.ensure_installed = {
+  "python",
+  "rust",
+}
+-- formatting
+local on_attach = function(client, bufnr)
+  -- Disable hover in favor of Pyright
+  client.server_capabilities.hoverProvider = false
+end
+
+require('lspconfig').ruff_lsp.setup {
+  on_attach = on_attach,
+}
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Auto select virtualenv Nvim open",
+  pattern = "*",
+  callback = function()
+    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+    if venv ~= "" then
+      require("venv-selector").retrieve_from_cache()
+    end
+  end,
+  once = true,
+})
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" }, }
+lvim.format_on_save.enabled = true
 
 lvim.plugins = {
   { "ishan9299/nvim-solarized-lua" },
@@ -50,5 +80,20 @@ lvim.plugins = {
     --  vim.o.timeoutlen = 500
     -- end
   },
-
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    opts = {
+      -- Your options go here
+      name = ".venv",
+      -- auto_refresh = false
+    },
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { "<leader>vs", "<cmd>VenvSelect<cr>" },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+    }
+  }
 }
