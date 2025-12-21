9103863c330e339f22f9552e836fa84e2980daee
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-12-21 20:40:45 +0100

    update lvim command

--- dotfiles/config/lvim/config.lua
+++ dotfiles/config/lvim/config.lua
@@ -5,7 +5,7 @@ local opts = { silent = true }
 vim.keymap.set("n", "<Up>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<Down>", ":bprevious<CR>", opts)
 
-vim.opt.guifont = "Inconsolata Nerd Font Mono:h18"
+vim.opt.guifont = "Inconsolata:h18"
 vim.opt.background = "light"
 
 -- improve python highlighting
@@ -78,7 +78,6 @@ lvim.plugins = {
 			},
 		},
 		lazy = false,
-		branch = "regexp",
 		keys = {
 			-- Keymap to open VenvSelector to pick a venv.
 			{ "<leader>vs", "<cmd>VenvSelect<cr>" },
@@ -106,64 +105,6 @@ lvim.plugins = {
 			format_on_save = {
 				timeout_ms = 1000,
 				lsp_fallback = true, -- Try LSP formatting if conform fails (optional)
-			},
-		},
-	},
-	{
-		"yetone/avante.nvim",
-		version = false, -- Never set this value to "*"! Never!
-		opts = {
-			-- add any opts here
-			-- for example
-			provider = "gemini",
-			gemini = {
-				-- endpoint = "https://api.openai.com/v1",
-				model = "gemini-2.0-flash", -- your desired model (or use gpt-4o, etc.)
-				timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-				temperature = 0.3,
-				max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-				--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-			},
-		},
-		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-		build = "make BUILD_FROM_SOURCE=true",
-		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- The below dependencies are optional,
-			"echasnovski/mini.pick", -- for file_selector provider mini.pick
-			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-			"ibhagwan/fzf-lua", -- for file_selector provider fzf
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"zbirenbaum/copilot.lua", -- for providers='copilot'
-			{
-				-- support for image pasting
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {
-					-- recommended settings
-					default = {
-						embed_image_as_base64 = false,
-						prompt_for_file_name = false,
-						drag_and_drop = {
-							insert_mode = true,
-						},
-						-- required for Windows users
-						use_absolute_path = true,
-					},
-				},
-			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
 			},
 		},
 	},
